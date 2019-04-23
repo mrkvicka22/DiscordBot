3552104afbfa1b4eb579025bab69fa720e913dab
mrkvicka22 <40635376+mrkvicka22@users.noreply.github.com>
2019-04-23 21:06:48 +0200

    DMs included + rozrobene classy
    Pridal som DMs a classu Shitler a classu Player. Obe rozrobene

--- carBot.py
+++ carBot.py
@@ -5,16 +5,22 @@ import time
 
 token = 'NTY0NDQxMjAzNTkzMDUyMTgw.XKttpA.hHp8b9GMYrsDmzYIXNoYLNRburw'
 
-bot = commands.Bot(command_prefix='!', status=discord.Status.idle, activity=discord.Game(name='Ressurecting Hitler. Using TOP SECRET encryption. Victim not known.'))
+bot = commands.Bot(command_prefix='!', status=discord.Status.idle, activity=discord.Game(name='Booting'))
 bot.remove_command('help')
 
+game_is_ongoing = False
 
 class Shitler():
 
     def __init__(self, participants):
         self.players = participants
-        
-        
+
+class Player():
+    
+    def __init__(self, role, disc_usr):
+        self.role = role
+        self.dsc_cls = disc_usr
+
 @bot.command()
 async def ping(ctx):
     await ctx.channel.send(f'My ping is {bot.latency * 1000}')
@@ -25,14 +31,23 @@ async def shitler(ctx, *args):
     players = []
     for player in args:
         if isinstance(player, discord.User):
-            players.append(a)
+            players.append(player)
 
     shitler_game = Shitler(players)
-    await ctx.channel.send(f'My ping is {bot.latency * 1000}')
+    await ctx.channel.send('The game has been set up')
 
+
+@bot.command(pass_context=True)
+async def create(ctx):
+    if game_is_ongoing:
+        await ctx.channel.send('The game is already in progress wait for it to finish!')
+        return 0
+    global player_list
+    player_list = []
 
 @bot.command()
-async def rndm(ctx, a, b):
-    await ctx.channel.send(str(random.randint(int(a),int(b))))
+async def rndm(ctx,  a, b):
+    author = ctx.message.author
+    await author.send(str(random.randint(int(a),int(b))))
 
 bot.run(token)
